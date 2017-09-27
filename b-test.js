163ccd69b452d1d835a986f573fb9ba4d7bf18ab
Claudio11 <claudiospuy@gmail.com>
2017-09-27 12:05:10 -0400

    Commit-to-have-conflict3333

--- b-test.js
+++ b-test.js
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-b test modified 7777.
-=======
-b test modified 111.
->>>>>>> refs/remotes/origin/master
-=======
-b test modified 111.
->>>>>>> origin/master
-=======
-b test modified 111.
->>>>>>> origin/master
-=======
-b test modified 111.
->>>>>>> origin/master
+chenged everything in file.
