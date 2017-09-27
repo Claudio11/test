98daf6eb9f2d745b33d0e99462a6a9175f951deb
Claudio11 <claudiospuy@gmail.com>
2017-09-27 11:46:10 -0400

    Commit-to-have-conflict3

--- b-test.js
+++ b-test.js
@@ -1,10 +1,14 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 b test modified 7777.
 =======
 b test modified 111.
 >>>>>>> refs/remotes/origin/master
+=======
+b test modified 111.
+>>>>>>> origin/master
 =======
 b test modified 111.
 >>>>>>> origin/master
