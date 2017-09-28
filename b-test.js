42f4e89c280035b0365adac7e5aebf176cf5926e
Claudio Soto <claudio@Claudios-MBP.fios-router.home>
2017-09-28 16:13:45 -0400

    b changed remotely

--- b-test.js
+++ b-test.js
@@ -1,2 +1,2 @@
-changed everything in file automatic normal merge strategy.
-changed everything in file 111.
+changed everything in file new one remote automatic normal merge strategy.
+changed everything in file 1119988776.
